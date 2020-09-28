d3e6a3256d2e9d23bc31e7145e32be431490303d
cobb <girldenzhang@gmail.com>
2020-09-28 16:13:52 +0800

    finish the test_b page

--- activation_tw_B.js
+++ activation_tw_B.js
@@ -67,38 +67,7 @@ $(".next1").click(function(){
 $(".next2").click(function(){
 
     if(checkInputs2() == 7) {
-        if (animating) return false;
-        animating = true;
-
-        current_fs = $(this).parent().parent().parent().parent();
-        next_fs = $(this).parent().parent().parent().parent().next();
-
-        //activate next step on progressbar using the index of next_fs
-        // $("#Activation_Steps li").eq($("fieldset").index(next_fs)).addClass("step--current");
-
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function (now, mx) {
-                //as the opacity of current_fs reduces to 0 - stored in "now"
-                //1. scale current_fs down to 80%
-                scale = 1 - (1 - now) * 0.2;
-                //2. bring next_fs from the right(50%)
-                // left = (now * 50)+"%";
-                //3. increase opacity of next_fs to 1 as it moves in
-                opacity = 1 - now;
-                current_fs.css({'transform': 'scale(' + scale + ')'});
-                next_fs.css({'left': left, 'opacity': opacity});
-            },
-            duration: 800,
-            complete: function () {
-                current_fs.hide();
-                animating = false;
-            },
-            //this comes from the custom easing plugin
-            easing: 'easeInOutBack'
-        })
+        connectcrmtw();
     }
 })
 
